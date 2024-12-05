0f8eb25e9e56dadd366475b3789ce4baa552718a
Hussain Ali <40995615+Thunder52@users.noreply.github.com>
2024-12-05 15:51:20 +0530

    Update api.js

--- Login-page/src/Service/api.js
+++ Login-page/src/Service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_API_URL = "https://api.algoarena.site/"
+const BACKEND_API_URL = "https://oj-project-yr3k.onrender.com/"
 
 const api = axios.create({
   baseURL: BACKEND_API_URL,
