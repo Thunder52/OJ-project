cc6171cb00ddc08c251105ad887479c06570c6e5
Hussain Ali <40995615+Thunder52@users.noreply.github.com>
2024-12-05 16:31:52 +0530

    Update UpdateProblem.jsx

--- Login-page/src/Components/UpdateProblem.jsx
+++ Login-page/src/Components/UpdateProblem.jsx
@@ -17,7 +17,7 @@ const UpdateProblemPage = () => {
   useEffect(() => {
     const fetchProblem = async () => {
       try {
-        const response = await axios.get(`https://api.algoarena.site/api/problems/${id}`);
+        const response = await axios.get(`https://oj-project-yr3k.onrender.com/api/problems/${id}`);
         setProblem(response.data);
       } catch (error) {
         console.error('Error fetching problem:', error);
